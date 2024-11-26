97e7365e8e4d6c4e367e0f2a7122fcaeec4929af
Lucas-ui <lucas7500@hotmail.fr>
2024-11-26 21:28:06 +0100

    Formulaire login et registration

--- Lucas/sandbox/src/views/Login.vue
+++ Lucas/sandbox/src/views/Login.vue
@@ -1,7 +1,44 @@
 <template>
   <p>Login</p>
+  <form action="" class="flex flex-col" @submit.prevent="login">
+    <label for="login">
+      Identifiant
+      <input
+        type="text"
+        id="login"
+        v-model="idLogin"
+        placeholder="Identifiant"
+      />
+    </label>
+    <label for="password">
+      Mot de passe
+      <input
+        type="password"
+        id="password"
+        v-model="password"
+        placeholder="Mot de passe"
+      />
+    </label>
+    <button @click="registration" class="w-fit" type="submit">
+      Se connecter !
+    </button>
+  </form>
 </template>
 
-<script></script>
+<script>
+export default {
+  data() {
+    return {
+      idLogin: "",
+      password: "",
+    };
+  },
+  methods: {
+    login() {
+      console.log(this.idLogin, this.password);
+    },
+  },
+};
+</script>
 
 <style></style>
--- Lucas/sandbox/src/views/Registration.vue
+++ Lucas/sandbox/src/views/Registration.vue
@@ -1,7 +1,83 @@
 <template>
   <p>Registration</p>
+  <form action="" class="flex flex-col" @submit.prevent="registration">
+    <label for="name">
+      Nom
+      <input type="text" id="name" v-model="name" placeholder="Nom" />
+    </label>
+    <label for="firstName">
+      Prénom
+      <input
+        type="text"
+        id="firstName"
+        v-model="firstName"
+        placeholder="Prénom"
+      />
+    </label>
+    <label for="email">
+      Email
+      <input type="email" id="email" v-model="email" placeholder="Email" />
+    </label>
+    <label for="login">
+      Identifiant
+      <input
+        type="text"
+        id="login"
+        v-model="loginRegister"
+        placeholder="Identifiant"
+      />
+    </label>
+    <label for="birth">
+      Date de naissance
+      <input
+        type="date"
+        id="birth"
+        v-model="birth"
+        placeholder="Date de naissance"
+      />
+    </label>
+    <label for="password">
+      Mot de passe
+      <input
+        type="password"
+        id="password"
+        v-model="password"
+        placeholder="Mot de passe"
+        minlength="8"
+        required
+      />
+    </label>
+    <button @click="registration" class="w-fit" type="submit">
+      Rejoindre Abend-core !
+    </button>
+  </form>
 </template>
 
-<script></script>
+<script>
+export default {
+  data() {
+    return {
+      name: "",
+      firstName: "",
+      email: "",
+      loginRegister: "",
+      birth: "",
+      password: "",
+    };
+  },
+  methods: {
+    registration() {
+      console.log(
+        this.name,
+        this.firstName,
+        this.email,
+        this.loginRegister,
+        this.birth,
+        this.password
+      );
+    },
+  },
+};
+</script>
 
 <style></style>
